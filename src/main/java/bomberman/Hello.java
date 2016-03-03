d4f9de9fde13ad8f456090faf449f3cb7e68a487
wahur666 <wahur13@inf.elte.hu>
2016-03-03 15:00:22 +0100

    Revert "BaseModel"
    This reverts commit bff9029eb698c6ee8528fca645d108b69ae05749.

--- src/main/java/bomberman/Hello.java
+++ src/main/java/bomberman/Hello.java
@@ -1,17 +1,9 @@
 package bomberman;
 
-import bomberman.models.Actor;
-import bomberman.models.Field;
-
-public class Hello{
-    public static void main(String[] args) {
-        Field f = new Field();
-        Actor[][] a = f.getField();
-        for (int i = 0; i < 15; i++) {
-            for (int j = 0; j < 15; j++) {
-                System.out.print("[" + a[i][j].getClass() + "]");
-            }
-            System.out.println("\n");
-        }
-    }
+public class Hello
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hi!");
+	}
 }
