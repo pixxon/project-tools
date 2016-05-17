4344d361547a27c674747561e1a06814c7e1577f
Szitár Gergő <szitar.gergo@gmail.com>
2016-05-17 15:25:16 +0200

    Docstrings added

--- src/main/java/bomberman/GameClient.java
+++ src/main/java/bomberman/GameClient.java
@@ -2,21 +2,26 @@ package bomberman;
 
 import bomberman.view.View;
 
-
 import java.util.concurrent.CountDownLatch;
-
 import javafx.application.Application;
-/* Created by Attila on 2016. 03. 14..
+
+/**
+ * GameClient class provides a user interface and connection with the server.
  */
-
-
 public class GameClient{
 
+	/**
+	 * Creates a new instance.
+	 */
 	public GameClient() {
-		CountDownLatch latch = new CountDownLatch(1);
 	    }
 
+	/**
+	 *
+	 * @param whereToConnect
+     */
     public void runClient(String whereToConnect) {
-	Application.launch(View.class, "");
+		View.connectionAddress = whereToConnect;
+		Application.launch(View.class, "");
     }
 }
--- src/main/java/bomberman/Hello.java
+++ src/main/java/bomberman/Hello.java
@@ -6,24 +6,56 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 
-
+/**
+ * Hello class is the main class for Bomberman game.
+ *
+ * <p>
+ *     Contains both the server and the client.
+ *     After running it the user can decide what to do.
+ * </p>
+ */
 public class Hello{
-    public static void main(String[] args) throws IOException {
+	/**
+	 * Main entry point for the Bomberman game.
+	 *
+	 * <p>
+	 *     The user can decide to start a server or a client.
+	 *     After that the IP address and the port is needed to start.
+	 * </p>
+	 *
+	 * @throws IOException When can not open System.in.
+     */
+    public static void main(String[]) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Server or client?");
 		String mode = br.readLine();
 
-	if ("server".equals(mode)){
-		GameServer server = new GameServer();
-		server.runServer(3000);
-		System.in.read();
-		server.stopServer();
-	}
+		if ("server".equals(mode)){
+			GameServer server = new GameServer();
 
-	if ("client".equals(mode)){
-		GameClient client = new GameClient();
-		client.runClient("ws://localhost:3000/bomberman/game");
-	}
+			System.out.println("IP address?");
+			String ip = br.readLine();
+			System.out.println("Port?");
+			Integer port = Integer.parseInt(br.readLine());
+
+			server.runServer(ip, port);
+
+			System.out.println("Press button to stop.");
+			System.in.read();
+
+			server.stopServer();
+		}
+
+		if ("client".equals(mode)){
+			GameClient client = new GameClient();
+
+			System.out.println("IP address?");
+			String ip = br.readLine();
+			System.out.println("Port?");
+			Integer port = Integer.parseInt(br.readLine());
+
+			client.runClient("ws://" + ip.toString() ":" + port.toString() + "/bomberman/game");
+		}
     }
 }
